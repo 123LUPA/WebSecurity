67414294ccebb7ebba3de1d93ce5741b22601805
Monica Toader <toadermonica@gmail.com>
2018-02-26 10:50:21 +0100

    pushing my branch
    smthg

--- app.js
+++ app.js
@@ -92,15 +92,7 @@ app.use('/users', userRouter);
 //     });
 // }
 
-// production error handler
-// no stacktraces leaked to user
-// app.use(function (err, req, res, next) {
-//     res.status(err.status || 500);
-//     res.render('error', {
-//         message: err.message,
-//         error: {}
-//     });
-// });
+
 
 // Swagger
 app.use('/api-docs', swagger.swaggerUi.serve, swagger.swaggerUi.setup(swagger.swaggerSpec));
