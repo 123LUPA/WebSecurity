9fc38a347b48021828e38d9d8d6af86d07b4b7ad
Lucia <Palkovicova.lucia@gmail.com>
2018-04-29 13:07:10 +0200

    routes

--- angular/src/app/new/new-password.service.ts
+++ angular/src/app/new/new-password.service.ts
@@ -2,17 +2,17 @@ import { Injectable } from '@angular/core';
 import {Router} from "@angular/router";
 import {HttpClient} from "@angular/common/http";
 import { ToastsManager } from 'ng2-toastr/ng2-toastr';
-
+import Config from "../../../app-config"
 @Injectable()
 export class NewPasswordService {
 
   constructor( private http: HttpClient, private push: ToastsManager, private router:Router) {}
 
-  private testUrl = 'http://localhost:3000/';
+  private testUrl = Config.nodeApi;
 
   changePassword(newPassword,token){
     this.http
-      .post(this.testUrl +'users/reset/'+ token, {password: newPassword}, )
+      .post(this.testUrl +'reset/'+ token, {password: newPassword}, )
       .subscribe(
         // Successful responses call the first callback.
         data => {
